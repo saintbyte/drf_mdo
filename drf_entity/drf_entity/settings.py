8ae077c165a0b590525ac692b77b89cf6350c521
Yegor Kazantsev <wwwadmin@bk.ru>
2022-12-21 13:04:38 +0300

    move to pg

--- drf_entity/drf_entity/settings.py
+++ drf_entity/drf_entity/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = "drf_entity.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mdo",
+        "USER": "postgres",
+        "PASSWORD": "change111",
+        "HOST": "localhost",
     }
 }
 
